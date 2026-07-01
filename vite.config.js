2d789e45150954bbf04c6454295e737a27c8bf18
nishant2608 <shukrenishant@gmail.com>
2026-07-01 19:17:11 +0530

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sahayog/', // Set the base path to the subdirectory where your app will be hosted
 })
